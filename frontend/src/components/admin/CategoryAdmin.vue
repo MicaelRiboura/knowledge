4a6f62085a66ba2a1b338e82d8d047b146557210
MicaelRiboura <53049385+MicaelRiboura@users.noreply.github.com>
2020-08-10 19:13:34 -0300

    knowledge

---
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="category-admin">
+    <b-form>
+      <input type="hidden" id="category-id" v-model="category.id" />
+      <b-form-group label="Nome:" label-for="category-name">
+        <b-form-input
+          id="category-name"
+          type="text"
+          v-model="category.name"
+          required
+          :readonly="mode==='remove'"
+          placeholder="Informe o nome da Categoria..."
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group label="Categoria Pai:" label-for="category-parentId">
+        <b-form-select
+          id="category-parentId"
+          v-if="mode==='save'"
+          :options="categories"
+          v-model="category.parentId"
+        ></b-form-select>
+        <b-form-input id="category-parentId" type="text" v-model="category.path" v-else readonly></b-form-input>
+      </b-form-group>
+      <b-row>
+        <b-col xs="12">
+          <b-button variant="primary" v-if="mode==='save'" @click="save">Salvar</b-button>
+          <b-button variant="danger" v-if="mode==='remove'" @click="remove">Excluir</b-button>
+          <b-button variant="secondary" @click="reset" class="ml-2">Cancelar</b-button>
+        </b-col>
+      </b-row>
+    </b-form>
+    <hr />
+    <b-table hover striped :items="categories" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadCategory(data.item,'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { baseApiUrl, showError } from "@/global";
+import axios from "axios";
+export default {
+  name: "CategoryAdmin",
+  data: function() {
+    return {
+      mode: "save",
+      category: {},
+      categories: [],
+      fields: [
+        { key: "id", label: "Código", sortable: true },
+        { key: "name", label: "Nome", sortable: true },
+        { key: "path", label: "Caminho", sortable: true },
+        { key: "actions", label: "Ações" }
+      ]
+    };
+  },
+  methods: {
+    loadCategories() {
+      const url = `${baseApiUrl}/categories`;
+      axios.get(url).then(res => {
+        // this.categories = res.data
+        this.categories = res.data.map(category => {
+          return { ...category, value: category.id, text: category.path };
+        });
+        // console.log(this.categories)
+      });
+    },
+    reset() {
+      this.mode = "save";
+      this.category = {};
+      this.loadCategories();
+    },
+    save() {
+      const method = this.category.id ? "put" : "post";
+      const id = this.category.id ? `${this.category.id}` : "";
+      axios[method](`${baseApiUrl}/categories/${id}`, this.category)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    remove() {
+      const id = this.category.id;
+      axios
+        .delete(`${baseApiUrl}/categories/${id}`)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    loadCategory(category, mode = "save") {
+      this.mode = mode;
+      this.category = { ...category };
+    }
+  },
+  mounted() {
+    this.loadCategories();
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/config/router.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '@/components/home/Home'
+import AdminPages from '@/components/admin/AdminPages'
+import ArticlesByCategory from '@/components/article/ArticlesByCategory'
+import ArticleById from '@/components/article/ArticleById'
+import Auth from '@/components/auth/Auth'
+import {userKey} from '@/global'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    name: 'home',
+    path: '/',
+    component: Home
+},
+{
+    name: 'adminPages',
+    path:'/admin',
+    component: AdminPages,
+    meta:{requiresAdmin: 'true'}
+},{
+    name:'articlesByCategory',
+    path: '/categories/:id/articles',
+    component:ArticlesByCategory
+},{
+    name: 'articleById',
+    path: '/articles/:id',
+    component: ArticleById
+},{
+    name: 'auth',
+    path: '/auth',
+    component: Auth
+}]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+router.beforeEach((to, from, next)=>{
+    const json = localStorage.getItem(userKey)
+    if(to.matched.some(record => record.meta.requiresAdmin=='true')){
+        const user = JSON.parse(json)
+        user && user.admin=='true' ? next() : next({path: '/'}) //Backend: admin está como sting e não boolean
+    } else{
+        next()
+    }
+})
+
+export default router
